cf8dc33dcfdadbead37b18d09bd727499c9b1e6e
spenguin <inof@soaringpenguin.com>
2026-03-25 15:38:59 -0700

    Style Sponsors presentation in footer.

--- includes/sponsorFns.class.php
+++ includes/sponsorFns.class.php
@@ -15,7 +15,8 @@ class sponsorFns
         $query = new WP_Query($args);
 
         if( $query->have_posts()): ?>
-            <div class="sponsor-list car--list max-wrapper__narrow">
+            <div class="sponsor-list card--list max-wrapper">
+                <h2>Rain City Comicon would like the following people and organisations for their support</h2>
                 <?php while( $query->have_posts() ): $query->the_post(); ?>
                     <div class="sponsor-list__item card--list__item">
                         <h3><?php the_title(); ?></h3>
